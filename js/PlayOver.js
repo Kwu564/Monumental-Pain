544e827865703aafcf01987b25347563b02276f5
jwynd <jacob.wynd@gmail.com>
2017-05-10 20:23:34 -0700

    added world transfers

--- js/PlayOver.js
+++ js/PlayOver.js
@@ -4,12 +4,14 @@
 // we do please log changes bellow, do not delete anything from the changelog. Include the date
 /*5/7/2017, KINDON
 /* Updated to include prefab with instantiated controls for topdown view, described inputs for prefab
-as they are currently, if you want to modify prefab, its spriteBuild.js
+ * as they are currently, if you want to modify prefab, its spriteBuild.js
+ *5/10/2017, JACOB
+ * Update so that entering town will start PlayPlatform state
 */
 
 
 var PlayOver = function(game) {
-   var map, layer1, layer2, layer3, player, wall, town;
+   var map, layer1, layer2, layer3, player, wall, town, townGroup, playerGroup;
 };
 PlayOver.prototype = {
    create: function() {
@@ -28,12 +30,16 @@ PlayOver.prototype = {
       map.setCollisionByExclusion([1], true, layer1);
       map.setCollisionByExclusion([3], true, layer2);
       
-      town = map.searchTileIndex(3, 0, false, layer2); //Doesn't work
-      
+      // town = map.searchTileIndex(3, 0, false, layer2); //Doesn't work
+      townGroup = this.game.add.group();
+      townGroup.enableBody = true;
+      town = map.createFromTiles([3], null, 'hero', layer2, townGroup); // Uses hero because it is the exact size of a tile
+      townGroup.alpha = 0;
+
       layer1.resizeWorld();
       
       //PREFAB SETUP
-      var playerGroup = this.game.add.group();
+      playerGroup = this.game.add.group();
       //parameters: (this.game,scaleX,scaleY,X position,Y position, asset key)
       player = new spritePlayOver(this.game,400,300,'hero');
       //this line adds the player onto the screen
@@ -41,7 +47,7 @@ PlayOver.prototype = {
       
       game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, .3, .3);
 
-      this.instructions = game.add.text(400, 32, "Arrow Keys to move, 'R' to switch states", {fontSize: "16px", fill: '#fff'});
+      this.instructions = game.add.text(400, 32, "Arrow Keys to move, enter town to see a new perspective", {fontSize: "16px", fill: '#fff'});
       this.instructions.anchor.set(0.5);
    },
    update: function() {
@@ -49,12 +55,14 @@ PlayOver.prototype = {
       
       game.physics.arcade.collide(player, layer1);
       game.physics.arcade.collide(player, layer2);
+      game.physics.arcade.overlap(player, townGroup, this.enterTown, null, this);
       
-      game.physics.arcade.overlap(player, town, this.enterTown, null, this); //Doesn't work
+      // game.physics.arcade.overlap(player, town, this.enterTown, null, this); //Doesn't work
       
-      if(game.input.keyboard.isDown(Phaser.Keyboard.R)){
+      // Uncomment below for quick state switching
+      /*if(game.input.keyboard.isDown(Phaser.Keyboard.R)){
          game.state.start('PlayPlatform');
-      }
+      }*/
    },
    enterTown: function() {
       game.state.start('PlayPlatform'); //doesn't work
--- js/PlayPlatform.js
+++ js/PlayPlatform.js
@@ -10,9 +10,12 @@ JUMPING, I had issues setting up collision with the prefab
 /* 5/9/2017, KIEFER
 Fixed the collision issues. Added player and ground as vars within the scope of PlayPlatform, so the hitGround check can find both objects. I also added gravity, because that wasn't implemented before.
 */
+/* 5/10/2017
+Added invisible gate at far left of world to return to the overworld
+*/
 
 var PlayPlatform = function(game) {
-    var player, ground;
+    var player, ground, exit;
 };
 PlayPlatform.prototype = {
    create: function() {
@@ -21,10 +24,18 @@ PlayPlatform.prototype = {
       this.world.width = 1600;
 
       ground = game.add.sprite(0, 400, 'platform');
-      ground.scale.setTo(2,1);
+      ground.scale.setTo(this.world.width/400, 1);
       game.physics.enable(ground, Phaser.Physics.ARCADE);
       ground.body.immovable = true;
       ground.alpha = 0; // make ground invisible so that player is pbg image
+
+      // EXIT GATE
+      exit = new spriteBuild(this.game, 1, 6.25, 1550, 200, 'platHero');
+      exit.anchor.setTo(0, 0);
+      exit.body.allowGravity = false;
+      exit.body.immovable = true;
+      exit.alpha = 0;
+
 
       //PREFAB SETUP
       var playerGroup = this.game.add.group();
@@ -32,18 +43,24 @@ PlayPlatform.prototype = {
       playerGroup.add(player);
       
       player.body.gravity.y = 600;
+      player.body.collideWorldBounds = true;
       
       game.camera.follow(player, Phaser.Camera.FOLLOW_PLATFORMER, .3, .3);
       
-      this.instructions = game.add.text(400, 32, "Arrow Keys to move, 'R' to switch states", {fontSize: "16px", fill: '#000'});
+      this.instructions = game.add.text(400, 32, "Arrow Keys to move, reach far right to return to world map", {fontSize: "16px", fill: '#000'});
       this.instructions.anchor.set(0.5);
 
    },
    update: function() {
-      //unable to put this line into prefab, still figuring it out
-      hitGround = game.physics.arcade.collide(player, ground);
-      if(game.input.keyboard.isDown(Phaser.Keyboard.R)){
+      game.physics.arcade.collide(player, ground);
+      game.physics.arcade.collide(player, ground);
+      // demonstration of another method of implementing gates
+      var hitExit = game.physics.arcade.collide(player, exit);
+      if (hitExit){
          game.state.start('PlayOver');
       }
+      /*if(game.input.keyboard.isDown(Phaser.Keyboard.R)){
+         game.state.start('PlayOver');
+      }*/
    }
 }
