76e59b38ad4e2db33b574d17c312e70d4448c0a4
jwynd <jacob.wynd@gmail.com>
2017-05-23 20:56:49 -0700

    forgot to actually add textData.js in last commit

---
+++ js/data/textData.js
@@ -0,0 +1,10 @@
+// An array file for all text boxes and related data
+
+const GLOBAL_TEXT_DATA = {
+   font: "bold Courier",
+   fontSize: "16px",
+   fill: 'black',
+   wordWrap: true,
+   wordWrapWidth: 800,
+   align: 'center'
+}
