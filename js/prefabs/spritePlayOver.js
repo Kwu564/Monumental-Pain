15cb41f524089cd15d6b9595cc850d7899de824c
Kwu564 <tnkboy4@gmail.com>
2017-06-03 04:41:40 -0700

    updated oworld hero sprite

--- js/prefabs/spritePlayOver.js
+++ js/prefabs/spritePlayOver.js
@@ -9,7 +9,8 @@ var spritePlayOver = function(game,x,y,src,frame){
 	console.log("spritePlayOver: create");
 	Phaser.Sprite.call(this,game,x,y,src,frame);
 
-    this.animations.add('OWorldHeroWalkSouth',[0, 1, 2], 7, true);
+    this.animations.add('OWorldHeroWalkSouth',[0, 1, 2, 3], 7, true);
+    this.animations.add('OWorldHeroWalkNorth',[4, 5, 6, 7], 7, true);
 
 	this.anchor.setTo(.5,.5);
 	this.game.physics.arcade.enableBody(this);
@@ -25,6 +26,7 @@ if(canEnter) { //only allow the player to move after they've been allowed to
     
     //Vertical movement checks
     if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
+        this.animations.play('OWorldHeroWalkNorth');
         //Check for diagonal movement, move slower
         if(game.input.keyboard.isDown(Phaser.Keyboard.D)) {
             this.body.velocity.y = -60;
@@ -34,7 +36,7 @@ if(canEnter) { //only allow the player to move after they've been allowed to
             this.body.velocity.x = -60;
         } else {
             this.body.velocity.x = 0;
-            this.body.velocity.y = -900;
+            this.body.velocity.y = -90;
         }
     } else if (game.input.keyboard.isDown(Phaser.Keyboard.S)){
         this.animations.play('OWorldHeroWalkSouth');
@@ -47,14 +49,14 @@ if(canEnter) { //only allow the player to move after they've been allowed to
             this.body.velocity.x = -60;
         } else {
             this.body.velocity.x = 0;
-            this.body.velocity.y = 900;
+            this.body.velocity.y = 90;
         }
     } else if(game.input.keyboard.isDown(Phaser.Keyboard.D)) {
         this.body.velocity.y = 0;
-        this.body.velocity.x = 900;
+        this.body.velocity.x = 90;
     } else if(game.input.keyboard.isDown(Phaser.Keyboard.A)) {
         this.body.velocity.y = 0;
-        this.body.velocity.x = -900;
+        this.body.velocity.x = -90;
     } else {
         this.frame = 1;
         this.body.velocity.x = 0;
