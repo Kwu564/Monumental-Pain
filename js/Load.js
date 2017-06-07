8f4615f32a53a81a5501341e82b393a6514a15e6
Kwu564 <tnkboy4@gmail.com>
2017-06-07 02:16:15 -0700

    added boss-bg to load

--- js/Load.js
+++ js/Load.js
@@ -38,7 +38,7 @@ Load.prototype = {
       
 
       // an array of keys and files; listed in pairs
-      let imgList = ['collider','collider.png','kevn-bg','kevn-bg.png','textbox','textbox.png','titleart','TitleArt.png', 'gameOver', 'gameOver.png','hatu-bg','hatu-bg.png'];
+      let imgList = ['collider','collider.png','kevn-bg','kevn-bg.png','textbox','textbox.png','titleart','TitleArt.png', 'gameOver', 'gameOver.png','hatu-bg','hatu-bg.png','boss-bg','boss-bg.png'];
       
       let sceneList = ['cutscene1','cutscene1.png'];
 
