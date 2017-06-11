ad3130968a253fb1cffabd7264eb855d982ae414
jwynd <jacob.wynd@gmail.com>
2017-06-10 22:58:26 -0700

    foemapBuild.js code cleaned

--- js/prefabs/foemapBuild.js
+++ js/prefabs/foemapBuild.js
@@ -1,6 +1,11 @@
-var foemapBuild = function(game,obj,enemySpots){
-	//console.log("foemapBuild: create");
-	Phaser.Sprite.call(this,game,obj.x,obj.y,'collider',0);
+/* foemapBuild.js
+ * 6/13/2017
+ * This file is a prefab for creating enemy spawnzones
+ * aka foemaps
+*/
+var foemapBuild = function(game, obj, enemySpots){
+    //console.log("foemapBuild: create");
+    Phaser.Sprite.call(this,game,obj.x,obj.y,'collider',0);
 
     this.size = obj.properties.size;
     // Type is where to start in the array of objects in the Tiled layer
@@ -9,7 +14,8 @@ var foemapBuild = function(game,obj,enemySpots){
     this.active = false;
     this.activeFoes = 0;
     
-    this.scale.set(obj.width,obj.height);
+    // set the size of the foemap
+    this.scale.set(obj.width, obj.height);
     
     this.enemySpots = [];
     for(let i = this.type; i < parseInt(this.size)+parseInt(this.type); i++) {
@@ -25,7 +31,7 @@ foemapBuild.prototype = Object.create(Phaser.Sprite.prototype);
 foemapBuild.prototype.constructor = foemapBuild;
 
 foemapBuild.prototype.update = function() {
-    if(this.activeFoes == 0) {
+    if(this.activeFoes === 0) {
        this.active = false;
     }
 }
